f5110c86312e97d922a5d96b7e7588cee0139cdc
Nuno362 <nuno.miguel.sousa12@gmail.com>
2023-05-06 15:28:29 +0100

    wishlist page2

--- lib/wishlistScreen.dart
+++ lib/wishlistScreen.dart
@@ -46,6 +46,12 @@ class _WishlistScreenState extends State<WishlistScreen>
     });
   }
 
+  void _deleteWishlist(int index) {
+    setState(() {
+      _wishlists.removeAt(index);
+    });
+  }
+
   void handleTabTap(int index) {
     switch (index) {
       case 0:
@@ -207,6 +213,181 @@ class _WishlistScreenState extends State<WishlistScreen>
         ),
       );
     }
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Pesquisar...',
+            hintStyle: Theme.of(context).textTheme.bodyMedium,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4.0),
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.outlineVariant,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4.0),
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.outlineVariant,
+              ),
+            ),
+            filled: true,
+            fillColor: Theme.of(context).colorScheme.surface,
+            prefixIcon: const Icon(Icons.search),
+            contentPadding: EdgeInsets.zero,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: IconButton(
+              onPressed: () {
+                // TODO: Implementar ação de conta do usuário
+              },
+              icon: Icon(
+                Icons.account_circle,
+                color: Theme.of(context).colorScheme.onBackground,
+                size: 36,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "As minhas listas",
+              style: Theme.of(context).textTheme.displaySmall,
+              textAlign: TextAlign.left,
+            ),
+            Text(
+              "${_wishlists.length} listas",
+              style: Theme.of(context).textTheme.bodyLarge,
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(height: 16.0),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: _wishlists.length,
+              itemBuilder: (context, index) {
+                final wishlist = _wishlists[index];
+                final wishlistName = wishlist['name'] as String;
+                final wishlistItems = wishlist['items'] as List<WishlistItem>;
+
+                return Card(
+                  child: ListTile(
+                    title: Text(wishlistName),
+                    subtitle:
+                        Text('Quantidade de itens: ${wishlistItems.length}'),
+                    onTap: () {
+                      // Handle list tap
+                    },
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          barrierDismissible: false,
+                          builder: (context) => Center(
+                            child: AlertDialog(
+                              title: const Text('Delete Wishlist'),
+                              content: Text(
+                                  'Are you sure you want to delete this wishlist?'),
+                              actions: [
+                                // Dialog actions will be added
+                                ElevatedButton(
+                                  onPressed: () {
+                                    _deleteWishlist(index);
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Delete'),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) => Center(
+                        child: AlertDialog(
+                          title: const Text('Create New Wishlist'),
+                          content: TextField(
+                            onChanged: (value) {
+                              _newWishlistName = value;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Wishlist Name',
+                            ),
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                _confirmNewWishlist(_newWishlistName);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Create'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancel'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  child: const Text('Create Wishlist'),
+                ),
+                const SizedBox(width: 16.0),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: _tabController,
+          indicator: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                width: 2.0,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ),
+          indicatorSize: TabBarIndicatorSize.tab,
+          labelColor: Theme.of(context).colorScheme.primary,
+          unselectedLabelColor: Theme.of(context).colorScheme.onSurface,
+          tabs: const [
+            Tab(icon: Icon(Icons.home_outlined)),
+            Tab(icon: Icon(Icons.category_outlined)),
+            Tab(icon: Icon(Icons.shopping_cart_outlined)),
+            Tab(icon: Icon(Icons.favorite_outline)),
+            Tab(icon: Icon(Icons.delivery_dining_outlined)),
+          ],
+          onTap: handleTabTap,
+        ),
+      ),
+    );
   }
 }
