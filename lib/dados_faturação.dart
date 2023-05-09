34d9ca05ff830ae7c2ea0815bae3f11854a566ca
Nuno362 <nuno.miguel.sousa12@gmail.com>
2023-05-09 21:06:28 +0100

    .

--- lib/dados_faturação.dart
+++ lib/dados_faturação.dart
@@ -33,7 +33,7 @@ class _BillingDataPageState extends State<BillingDataPage> {
             Column(
               children: [
                 SvgPicture.asset(
-                  'assets/imagens/128-5-morada.svg',
+                  'assets/imagens/128-5-moradappp.svg',
                   width: 350,
                   height: 350,
                 ),
